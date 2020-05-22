cc065827ac62b296e7d029065253ecf326f73892
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-05-22 16:29:52 +0200

    work page, button, scroll, bg

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//ALTERNATIVE LOADER
 /*
 setTimeout(function(){
             window.location.href = 'home.html';
@@ -11,3 +12,26 @@ var overlay = document.getElementById("loader-wrapper");
 window.addEventListener("load", function(){
     overlay.style.display = "none";
 })
+
+
+//GO TO TOP BTN//
+
+//enable/disable scroll button based on scroller position
+function scrollFunction() {
+    var topBtn = document.getElementById("topBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+}
+
+window.onscroll = function () {
+    scrollFunction()
+};
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() { // eslint-disable-line no-unused-vars
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
