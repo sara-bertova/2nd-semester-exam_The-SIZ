e6a72fd071449a7b28f947b91a7cf9c543738ac9
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-05-22 15:40:30 +0200

    loader

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+/*
 setTimeout(function(){
             window.location.href = 'home.html';
          }, 2000);
+*/
+
+
+//LOADER
+var overlay = document.getElementById("loader-wrapper");
+
+window.addEventListener("load", function(){
+    overlay.style.display = "none";
+})
