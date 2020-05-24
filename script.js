7c3024830b5d44f575cd722e8a1085692b7cd6c0
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-05-24 18:50:08 +0200

    photoshoot page --> full-size image modal

--- script.js
+++ script.js
@@ -5,14 +5,12 @@ setTimeout(function(){
          }, 2000);
 */
 
-
 //LOADER
 var overlay = document.getElementById("loader_wrapper");
 
 window.addEventListener("load", function () {
     overlay.style.display = "none";
 })
-
 
 //GO TO TOP BTN
 var topBtn = document.getElementById("topBtn");
@@ -36,3 +34,14 @@ function topFunction() { // eslint-disable-line no-unused-vars
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//MODAL
+const modal = document.querySelector(".modal-background");
+modal.addEventListener("click", () => {
+    modal.classList.add("hide");
+});
+
+const modalImg = document.querySelector(".modalImg")
+modalImg.addEventListener("click", () => {
+    modal.classList.remove("hide");
+});
