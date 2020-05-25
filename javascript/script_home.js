695047ccab5154b4dc14289dd14057c0a0e60934
zuzanatrembulakova <zuzanatrembulakova@users.noreply.github.com>
2020-05-25 08:48:04 +0200

    homepage data fetched

--- javascript/script_home.js
+++ javascript/script_home.js
@@ -7,10 +7,23 @@ fetch("http://www.rasbery.eu/2nd-semester-exam/wp-json/wp/v2/homepage_photo?per_
     })
 
 function handleHomepageData(jsonData) {
+    jsonData.reverse();
     jsonData.forEach(showPhoto);
 }
 
-function showPhoto(photo) {
-    console.log(photo.title.rendered)
+function showPhoto(onePhoto) {
+
+    const template = document.querySelector("template").content;
+    const copy = template.cloneNode(true);
+
+    const homepageImg = document.createElement("img");
+
+    if (onePhoto.number <= 3){
+        console.log(onePhoto.title.rendered);
+        homepageImg.src = onePhoto.photo.guid;
+        copy.querySelector(".homepage_1column").append(homepageImg);
+    }
+
+    document.querySelector(".homepage_container").appendChild(copy);
 }
 
