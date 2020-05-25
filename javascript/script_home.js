a6495882f73133385636351d4a7f30482d3bd1a3
zuzanatrembulakova <zuzanatrembulakova@users.noreply.github.com>
2020-05-25 20:08:59 +0200

    nav menu name added

--- javascript/script_home.js
+++ javascript/script_home.js
@@ -16,45 +16,29 @@ function showPhoto(onePhoto) {
     const homepageImg = document.createElement("img");
     const quote = document.createElement("p");
 
-    if (onePhoto.number <= 3){
-        homepageImg.src = onePhoto.photo.guid;
-        document.querySelector(".homepage_1column").append(homepageImg);
-    }
+    homepageImg.src = onePhoto.photo.guid;
 
-    if (onePhoto.number <= 6 && onePhoto.number >=4 ){
+    if (onePhoto.number <= 3){
+        document.querySelector(".homepage_1column").append(homepageImg);
+    } else if (onePhoto.number <= 6){
         if (onePhoto.number == 5){
             quote.textContent = "Dreamy";
             quote.classList.add("homepage_quote1");
             document.querySelector(".homepage_2column").append(quote);
         }
-
-        homepageImg.src = onePhoto.photo.guid;
         document.querySelector(".homepage_2column").append(homepageImg);
-    }
-
-    if (onePhoto.number <= 11 && onePhoto.number >=7 ){
-        homepageImg.src = onePhoto.photo.guid;
+    } else if (onePhoto.number <= 11){
         document.querySelector(".homepage_3column").append(homepageImg);
-    }
-
-    if (onePhoto.number <= 14 && onePhoto.number >=12 ){
-        homepageImg.src = onePhoto.photo.guid;
+    } else if (onePhoto.number <= 14){
         document.querySelector(".homepage_4column").append(homepageImg);
-    }
-
-    if (onePhoto.number <= 17 && onePhoto.number >=15 ){
+    } else if (onePhoto.number <= 17){
         if (onePhoto.number == 16){
             quote.textContent = "Mysterious";
             quote.classList.add("homepage_quote2");
             document.querySelector(".homepage_5column").append(quote);
         }
-
-        homepageImg.src = onePhoto.photo.guid;
         document.querySelector(".homepage_5column").append(homepageImg);
-    }
-
-    if (onePhoto.number <= 19 && onePhoto.number >=18 ){
-        homepageImg.src = onePhoto.photo.guid;
+    } else if (onePhoto.number <= 19){
         document.querySelector(".homepage_6column").append(homepageImg);
     }
 
