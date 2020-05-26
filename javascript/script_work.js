b7411702c2756182a1baa22e14aad5c2142b6587
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-05-26 10:55:56 +0200

    submenu

--- javascript/script_work.js
+++ javascript/script_work.js
@@ -1,10 +1,37 @@
+//SUBMENU
+fetch("http://www.rasbery.eu/2nd-semester-exam/wp-json/wp/v2/categories?parent=5")
+    .then(function (response) {
+        return response.json()
+    })
+    .then(function (data) {
+        handleSubmenuData(data)
+    })
+
+function handleSubmenuData(jsonData) {
+    jsonData.reverse();
+    jsonData.forEach(showSubmenu);
+}
+
+function showSubmenu(oneCategory) {
+    const ul = document.querySelector("#submenu")
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.textContent = oneCategory.name;
+    a.classList.add("menu_other");
+    li.appendChild(a);
+    ul.appendChild(li);
+}
+
+
 //MODAL
 const modal = document.querySelector(".modal-background");
-modal.addEventListener("click", () => {
-    modal.classList.add("hide");
-});
+if (modal) {
+    modal.addEventListener("click", () => {
+        modal.classList.add("hide");
+    });
 
-const modalImg = document.querySelector(".modalImg")
-modalImg.addEventListener("click", () => {
-    modal.classList.remove("hide");
-});
+    const modalImg = document.querySelector(".modalImg")
+    modalImg.addEventListener("click", () => {
+        modal.classList.remove("hide");
+    });
+}
